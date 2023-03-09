ed5794f2ba805cb4221f31185251f256c8e661f7
Reeta Olivier <reeta_olivier@hotmail.co.uk>
2023-03-09 16:01:07 -0300

    All links working

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,13 +8,18 @@ class BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.movie_id = params[:bookmark][:movie_id]
-    raise
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
       render "lists/show", status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 
   # get "/bookmarks/new", to: "bookmarks#new"
   # post "/bookmarks", to: "bookmarks#create"
