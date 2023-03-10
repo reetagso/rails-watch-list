947cfd5dd6a279535bbc27fc4f99788ba5455609
Reeta Olivier <reeta_olivier@hotmail.co.uk>
2023-03-10 15:40:41 -0300

    Show image on bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -29,7 +29,7 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, photos: [])
+    params.require(:bookmark).permit(:comment, :photo)
   end
 
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  has_many_attached :photos
+  has_one_attached :photo
   validates :comment, presence: true, length: { minimum: 6 }
   validates :movie_id, uniqueness: {scope: :list_id }
 end
